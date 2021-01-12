0f6ae9ab201e06e83f9cdd55787cc93fa5b62797
Georges Aaron RANDRIANAINA <randria-georges@outlook.com>
2021-01-12 20:32:06 +0100

    add(ampianaL): merging two lists using the rules from ampiana

--- sikidy.pl
+++ sikidy.pl
@@ -2,3 +2,10 @@
 ampiana(X, X, 2).
 ampiana(_, _, 1).
 
+ampianaL([X1, X2, X3, X4],
+	  [Y1, Y2, Y3, Y4],
+	  [A, B, D, E]) :-
+	ampiana(X1, Y1, A),
+	ampiana(X2, Y2, B),
+	ampiana(X3, Y3, D),
+	ampiana(X4, Y4, E).
